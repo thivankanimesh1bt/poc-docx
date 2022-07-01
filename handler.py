ea60139eaf46c9846c5810ca469b2b8edb6720e1
thivankanimesh1bt <thivanka.nimesh@1billiontech.com>
2022-07-01 14:46:17 +0530

    updated

--- handler.py
+++ handler.py
@@ -770,14 +770,9 @@ def docxGenerate(event, context):
              
     }
 
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": "Success"
     }
 
     replace_txt(dataDump["text_replaces"], doc)
